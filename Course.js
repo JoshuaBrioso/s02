5ebd63f4b760deda846be80430b6503d924b7bba
JoshuaBrioso <107360131+JoshuaBrioso@users.noreply.github.com>
2022-09-20 15:25:56 +0800

    Add files via upload
    Add Activity Code

---
+++ Course.js
@@ -0,0 +1,41 @@
+[
+{
+ "_id" : "1-321-143-775",
+    "name" : "Joshua",
+    "description" : "About to beat some bullies",
+    "price" : 1200,
+    "slots" : 20,
+    "schedule" : "10:00 am - 1:00 pm",
+    "instructor" : "Jermon Batista",
+    "isActive" : true,
+    "enrollees" : [ 
+        {
+            "enrollee_id" : "123",
+            "userId" : "1-321-143-775",
+            "userName" : "student 1",
+            "isPaid" : true,
+            "dateEnrolled" : "20/09/22"        }
+    ]
+},
+
+
+{
+   "_id" : "2-322-144-776",
+    "name" : "Herson",
+    "description" : "Looking Forward to a brighter future",
+    "price" : 3000,
+    "slots" : 30,
+    "schedule" : "11:00 am - 1:00 pm",
+    "instructor" : "Optimus Prime",
+    "isActive" : true,
+    "enrollees" : [ 
+        {
+            "enrollee_id" : "321",
+            "userId" : "2-322-144-776",
+            "userName" : "student 2",
+            "isPaid" : true,
+            "dateEnrolled" : "20/09/21"
+        }
+    ]
+}
+]
---
+++ User.js
@@ -0,0 +1,42 @@
+[
+    {
+         "_id" : "1-321-143-775",
+            "username" : "student 1",
+            "firstName" : "Joshua",
+            "lastName" : "Brioso",
+            "email" : "brioso12345@gmail.com",
+            "password" : "Nalate12345",
+            "mobileNumber" : "0123456789",
+            "isAdmin" : false,
+            "enrollments" : [ 
+                {
+                    "enrollment_id" : "123",
+                    "course_id" : "123",
+                    "courseName" : "Judo",
+                    "isPaid" : true,
+                    "dateEnrolled" : "20/09/22"
+            }
+        ]
+    },
+
+
+    {
+        "_id" : "2-322-144-776",
+            "username" : "student 2",
+            "firstName" : "Herson",
+            "lastName" : "Burador",
+            "email" : "HersonPogi123@gmail.com",
+            "password" : "Mechs&Chads123",
+            "mobileNumber" : "09247129818",
+            "isAdmin" : false,
+            "enrollments" : [ 
+                {
+                    "enrollment_id" : "2",
+                    "course_id" : "2",
+                    "courseName" : "Robotics",
+                    "isPaid" : true,
+                    "dateEnrolled" : "20/09/21"
+            }
+        ]
+    }
+]
